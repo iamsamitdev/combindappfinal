057d0c284f51fc9d656843781aea7b4eecf58766
Samit Koyom <samitkoyom@gmail.com>
2018-07-07 14:56:48 +0700

    update api 2

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -44,7 +44,23 @@ export class RegisterPage {
   }
 
   signup(){
-    console.log(this.userData);
+    //console.log(this.userData);
+    this.webapi.postData(this.userData,'register.php').then((result)=>{
+      this.responseData = result;
+      //console.log(this.responseData);
+      if(this.responseData.userData){
+        let alert = this.alertCtrl.create({
+          title:"สถานะการลงทะเบียน",
+          subTitle:"ลงทะเบียนเรียบร้อยแล้ว",
+          buttons:['Dismiss']
+        });
+        alert.present();
+        // ส่งกลับไปหน้าหลัก (dashboard)
+        this.navCtrl.setRoot(TabsPage);
+      }
+    },(err)=>{
+      console.log(err);
+    });
   }
 
 }
