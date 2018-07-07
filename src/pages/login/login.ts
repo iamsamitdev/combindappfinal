60144e03dbda213ad4992429dfd3ea478fc8324b
Samit Koyom <samitkoyom@gmail.com>
2018-07-07 15:30:59 +0700

    update login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App, AlertController } from 'ionic-angular';
 import { RegisterPage } from '../register/register';
 import { TabsPage } from '../tabs/tabs';
+import { WebapiServiceProvider } from '../../providers/webapi-service/webapi-service';
 
 @IonicPage()
 @Component({
@@ -10,7 +11,21 @@ import { TabsPage } from '../tabs/tabs';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public app:App) {
+   // กำหนดตัวแปรผูกฟอร์ม (Model)
+   userData = {
+    "username":"",
+    "password":""
+  }
+
+  // ตัวแปรรับข้อมูลจาก api
+  responseData:any;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public app:App,
+    public alertCtrl: AlertController,
+    public webapi: WebapiServiceProvider) {
   }
 
   ionViewDidLoad() {
@@ -24,6 +39,34 @@ export class LoginPage {
   gotoDashboard()
   {
     this.navCtrl.setRoot(TabsPage);
+  }
+
+  login()
+  {
+    //console.log(this.userData);
+    this.webapi.postData(this.userData,'login.php').then((result)=>{
+      this.responseData = result;
+      //console.log(this.responseData);
+      if(this.responseData.userData){
+        let alert = this.alertCtrl.create({
+          title:"สถานะการเข้าระบบ",
+          subTitle:"เข้าระบบเรียบร้อยแล้ว",
+          buttons:['Dismiss']
+        });
+        alert.present();
+        // ส่งกลับไปหน้าหลัก (dashboard)
+        this.navCtrl.setRoot(TabsPage);
+      }else{
+        let alert = this.alertCtrl.create({
+          title:"มีข้อผิดพลาด",
+          subTitle:"เข้าระบบไม่ถูกต้อง ลองใหม่",
+          buttons:['Dismiss']
+        });
+        alert.present();
+      }
+    },(err)=>{
+      console.log(err);
+    });
   }
 
 }
